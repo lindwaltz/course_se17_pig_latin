0738c7880b6b7bbf36052df9d01064afafd190cf
Martin Svenson <martin@lindvall.at>
2017-02-20 17:29:12 +0100

    another attempt

--- build/deploy-ghpages.sh
+++ build/deploy-ghpages.sh
@@ -4,14 +4,13 @@ set -e
 # show where we are on the machine
 pwd
 remote=$(git config remote.origin.url)
-username=$(git config user.name)
-usermail=$(git config user.email)
+#username=$(git config user.name)
+#usermail=$(git config user.email)
 
 mkdir gh-pages-branch
 git clone ./ ./gh-pages-branch
 cd gh-pages-branch
-git config --local user.name $username
-git config --local user.email $usermail
+git config --local user.name "lindwaltz"
 git remote add --fetch github "$remote"
 git checkout gh-pages
 git rm -rf .
