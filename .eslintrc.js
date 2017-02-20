7bc6125294e81507b71013528274d4f5e6930f8d
Martin Svenson <martin@lindvall.at>
2017-02-20 14:37:31 +0100

    modified ESlint conf

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,8 +23,17 @@ module.exports = {
     'generator-star-spacing': 0,
     // allow skipping spaces
     'space-before-function-paren': 0,
-    // unused vars are used in dev. allow it as WARN.
+
+    // use camelcase naming
+    'camelcase': 0,
+    
+    // allow as WARN because it's used during dev.
     'no-unused-vars': 1,
+    'comma-dangle': 1,
+
+    // more settings that are more appropriate as WARN
+    'no-trailing-spaces': 1,
+    'padded-blocks': 1,
 
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
