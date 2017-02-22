285f06099e7f77f481bdeeceff0261d75abd0c6a
Martin Svenson <martin@lindvall.at>
2017-02-22 14:35:00 +0100

    added rovarspraket. also fixed infinite loop

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,22 @@
     <div class="hello">
       <h1>{{ msg }}</h1>
       <h2>The Joyous Pig Latin Generator</h2>
-      <div class="textinput">
-        <p>Input:</p>
-        <textarea v-model='piginput' rows="4" cols="80" />
+      <div class="pigconvert">
+        <div class="textinput">
+          <textarea v-model='piginput' rows="10" cols="60" />
+        </div>
+        <!-- <button v-on:click="piggify()">Piggify!</button> -->
+        <div class="textoutput">
+          <p><i>{{ pigoutput }}</i></p>
+        </div>
       </div>
-      <button v-on:click="piggify()">Piggify!</button>
-      <div class="textoutput">
-        <p><i>{{ pigoutput }}<i></p>
+      <div class="settings">
+        <input type="radio" id="piglatin" value="piglatin" v-model="picked">
+        <label for="piglatin">Pig Latin</label>
+        <br>
+        <input type="radio" id="rovarspraket" value="rovarspraket" v-model="picked">
+        <label for="rovarspraket">Rövarspråket</label>
+        <br>
       </div>
       <hr />
       <p>This is a demonstration project of a CI-pipeline. <br /> made as part of the WASP software engineering course</p>
@@ -21,12 +30,13 @@
 <script>
 import pigspeak from './lib/pigspeak'
 
-export default {
+const comp = {
   name: 'app',
 
   data () {
     return {
       msg: 'Porky',
+      picked: 'piglatin',
       piginput: 'Write your message',
       pigoutput: 'Output will appear here'
     }
@@ -34,10 +44,27 @@ export default {
 
   methods: {
     piggify() {
-      this.pigoutput = pigspeak.piggify(this.piginput)
+      let newmsg = ''
+      if (this.picked === 'piglatin') {
+        newmsg = pigspeak.piggify(this.piginput)
+      } else if (this.picked === 'rovarspraket') {
+        newmsg = pigspeak.rovarify(this.piginput)
+      }
+      this.pigoutput = newmsg
+    }
+  },
+
+  watch: {
+    'piginput': function (val, old) {
+      this.piggify()
+    },
+    'picked': function (val, old) {
+      this.piggify()
     }
   }
 }
+
+export default comp
 </script>
 
 <style>
@@ -69,5 +96,25 @@ li {
 
 a {
   color: #42b983;
+}
+
+.pigconvert {
+  display: -webkit-inline-flex;
+  display: inline-flex;
+  max-width: 800px;
+}
+
+button {
+  max-height: 50px;
+  margin: 10px;
+}
+
+.textinput {
+  margin: 10px;
+}
+
+.textoutput {
+  min-width: 200px;
+  max-width: 400px;
 }
 </style>
